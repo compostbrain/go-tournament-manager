7f13d1984532edffd5ba271ea7ac70a67935bca9
compostbrain <joel@wildmeadowsfarm.com>
2017-09-18 11:58:56 -0400

    Fixed rubocop offenses

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,4 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   validates_presence_of :first_name, :last_name
-
 end
--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,11 +1,10 @@
-require 'rails_helper'
+require "rails_helper"
 
-describe 'navigate' do
-  describe 'homepage' do
-    it 'can be reached successfully' do
+describe "navigate" do
+  describe "homepage" do
+    it "can be reached successfully" do
       visit root_path
       expect(page.status_code).to eq 200
     end
   end
-
 end
