c6f99a735072fecaa517d8b58997dd2d6694c494
compostbrain <joel@wildmeadowsfarm.com>
2017-09-18 18:42:27 -0400

    Implemented Tournament creation

--- spec/features/tournament_spec.rb
+++ spec/features/tournament_spec.rb
@@ -1,12 +1,34 @@
 require "rails_helper"
 
-describe "#index" do
-  it "can be reached successfully" do
-    visit tournaments_path
-    expect(page.status_code).to eq(200)
+describe 'navigate' do
+  describe "#index" do
+    it "can be reached successfully" do
+      visit tournaments_path
+      expect(page.status_code).to eq(200)
+    end
+    it "has a title of Tournaments" do
+      visit tournaments_path
+      expect(page).to have_content(/Tournaments/)
+    end
   end
-  it "has a title of Tournaments" do
-    visit tournaments_path
-    expect(page).to have_content(/Tournaments/)
+
+  describe 'creation' do
+    it 'has a new form that can be reached' do
+      visit new_tournament_path
+      expect(page.status_code).to eq 200
+    end
+    it 'can be created from new form page' do
+      visit new_tournament_path
+      fill_in 'tournament[name]', with: "Return of the Jedi"
+      fill_in 'tournament[location]', with: "Saturns Rings"
+      fill_in 'tournament[director]', with: "Humpty Dumpty"
+      fill_in 'tournament[begin_date]', with: Date.today
+      fill_in 'tournament[end_date]', with: Date.tomorrow
+
+      click_on "Save"
+
+      expect(page).to have_content("Return of the Jedi")
+    end
+
   end
 end
