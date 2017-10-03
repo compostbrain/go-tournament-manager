261ce66d5af7c2cbfb63c8b7791f609b562a7262
compostbrain <joelcahalan@gmail.com>
2017-10-03 14:07:05 -0400

    sidenav editing

--- app/models/tournament_director.rb
+++ app/models/tournament_director.rb
@@ -1,6 +1,4 @@
 class TournamentDirector < User
-
-
   def full_name
     first_name.upcase + " " + last_name.upcase
   end
--- spec/factories/tournaments.rb
+++ spec/factories/tournaments.rb
@@ -2,8 +2,8 @@ FactoryGirl.define do
   factory :tournament do
     name "Bun Yum Invitational"
     location "Washington D.C."
-    director "Ki Jie"
     begin_date "2017-09-18"
     end_date "2017-09-18"
+    user
   end
 end
