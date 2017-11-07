f93fc744e9049ea8ca1c7eada776a96f66b1c7d1
compostbrain <joelcahalan@gmail.com>
2017-11-06 20:13:38 -0500

    standings controller

--- app/models/player.rb
+++ app/models/player.rb
@@ -42,6 +42,10 @@ class Player < ApplicationRecord
 >>>>>>> standings controller
   end
 
+  def self.with_final_registration_statuses(tournament)
+  Player.joins(:tournament_registrations).where(tournament_registrations: {tournament_id: tournament.id, status: "final"})
+  end
+
   def self.sorted_by_rating
     players = Players.all
     players.sort_by(&:rating)
