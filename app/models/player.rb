f6081ace3e7d19a5e6e832c29637a352aebaccdc
compostbrain <joelcahalan@gmail.com>
2017-11-06 20:13:38 -0500

    standings controller

--- app/models/player.rb
+++ app/models/player.rb
@@ -6,6 +6,10 @@ class Player < ApplicationRecord
   accepts_nested_attributes_for :round_statuses
   attr_accessor :tournament_points, :previous_opponents
 
+  def self.with_final_registration_statuses(tournament)
+  Player.joins(:tournament_registrations).where(tournament_registrations: {tournament_id: tournament.id, status: "final"})
+  end
+
   def self.sorted_by_rating
     players = Players.all
     players.sort_by(&:rating)
--- config/routes.rb
+++ config/routes.rb
@@ -24,4 +24,6 @@ Rails.application.routes.draw do
 
   resources :games do
   end
+  get "tournaments/:tournament_id/standings",
+  to: "standings#index", as: :tournament_standings
 end
